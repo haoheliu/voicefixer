879e6ac028049d3bddb948757aaee6b76d5a7746
haoheliu <867390095@qq.com>
2021-10-26 22:57:22 +0800

    fix bug

--- voicefixer/restorer/__init__.py
+++ voicefixer/restorer/__init__.py
@@ -26,5 +26,5 @@ if (not os.path.exists(meta["voicefixer_fe"]['path'])):
     cmd = "wget https://zenodo.org/record/5469951/files/epoch%3D15_trimed_bn.ckpt?download=1 -O " + \
           meta["voicefixer_fe"]['path']
     os.system(cmd)
-    temp = torch.load("/Users/liuhaohe/.cache/voicefixer/analysis_module/checkpoints/epoch=15_trimed_bn.ckpt")
+    temp = torch.load(meta["voicefixer_fe"]['path'])
     torch.save(temp['state_dict'], os.path.join(os.path.expanduser('~'), ".cache/voicefixer/analysis_module/checkpoints/vf.ckpt"))
