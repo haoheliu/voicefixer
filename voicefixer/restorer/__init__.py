21b46cc0b8bea0eff7b3f2b219958e442c94b68a
ZFTurbo <ZF-Turbo@yandex.ru>
2021-12-03 13:31:55 +0300

    Change wget to urllib.request

--- voicefixer/restorer/__init__.py
+++ voicefixer/restorer/__init__.py
@@ -12,6 +12,7 @@
 
 import os
 import torch
+import urllib.request
 
 meta = {
     "voicefixer_fe": {
@@ -23,7 +24,14 @@ meta = {
 if (not os.path.exists(meta["voicefixer_fe"]['path'])):
     os.makedirs(os.path.dirname(meta["voicefixer_fe"]['path']), exist_ok=True)
     print("Downloading the main structure of voicefixer")
-    cmd = "wget "+ meta["voicefixer_fe"]['url'] + " -O " + meta["voicefixer_fe"]['path']
-    os.system(cmd)
+
+    urllib.request.urlretrieve(
+        meta["voicefixer_fe"]['url'],
+        meta["voicefixer_fe"]['path']
+    )
+    print('Weights downloaded in: {} Size: {}'.format(meta["voicefixer_fe"]['path'], os.path.getsize(meta["voicefixer_fe"]['path'])))
+
+    # cmd = "wget "+ meta["voicefixer_fe"]['url'] + " -O " + meta["voicefixer_fe"]['path']
+    # os.system(cmd)
     # temp = torch.load(meta["voicefixer_fe"]['path'])
     # torch.save(temp['state_dict'], os.path.join(os.path.expanduser('~'), ".cache/voicefixer/analysis_module/checkpoints/vf.ckpt"))
